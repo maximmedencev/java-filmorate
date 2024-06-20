0ea2b740baf7e21a278b48593cdd4bd57a0bef51
Maxim Medencev <maxim.medencev@yandex.ru>
2024-06-21 00:18:28 +0700

    Исправить checkstyle error

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,15 +2,14 @@ package ru.yandex.practicum.filmorate.model;
 
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.Set;
 
 @Builder
